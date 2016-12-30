d4469f6a06dd732eb5186c5f4bdd840ee787455a
Joe Noonan <joenoonan27@gmail.com>
2016-12-30 14:45:58 -0600

    edit=true now works

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.onload = function() {
 
-  window.default_color_sequence = "red,orange,yellow,green,blue,indigo,red,orange,yellow,green,blue,indigo"
+  window.default_color_sequence = "red,orange,yellow,green,blue,indigo,red,orange,yellow,green,blue,indigo";
 
   function getColors(){
     return window.color_sequence.replace(/ /g, '');
@@ -11,15 +11,15 @@ window.onload = function() {
       document.querySelector("[data-id='output-html']").innerHTML = '';
     }
     else{
-
       // only add a color string if color is not default
-      let color_sequence_for_uri = ''
+      let color_sequence_for_uri = '';
       if(colors != window.default_color_sequence){
-        color_sequence_for_uri = encodeURIComponent(`<wbr>&color_sequence=${colors}`)
+        color_sequence_for_uri = `&color_sequence=${encodeURIComponent(colors)}`;
       }
 
-      document.querySelector("[data-id='output-html']").innerHTML = document.location +
-        `?<wbr>font-size=${font_size}` + color_sequence_for_uri + `<wbr>&font-family=${font_family.replace(/ /g, '%20')}` + `<wbr>&message=${encodeURIComponent(message)}`;
+      message_sequence_for_uri = `&message=${encodeURIComponent(encodeURIComponent(message))}`;
+      document.querySelector("[data-id='output-html']").innerHTML = document.location.origin + document.location.pathname +
+        `?font-size=${font_size}` + color_sequence_for_uri + `&font-family=${font_family.replace(/ /g, '%20')}` + message_sequence_for_uri;
     }
   }
 
@@ -70,7 +70,7 @@ window.onload = function() {
     document.querySelector("[data-id='colorful-text'] pre").style.fontFamily = style;
   }
 
-  // make page display text
+  // option 1: no parameters -> make page display text
     if(document.location.search === '') {
       // show page specific
       document.querySelector("[data-id='controls']").style.display = 'block';
@@ -78,6 +78,8 @@ window.onload = function() {
       document.querySelector("[data-id='colorful-text'] pre").style.fontFamily = getFontFamily();
       window.color_sequence = window.default_color_sequence;
     }
+
+    // option 2: all parameters except no url param "edit=true"
     else
     {
       // edit page specific
@@ -85,7 +87,9 @@ window.onload = function() {
 
       var font_size = '';
       try{
-        font_size = uri.match(/font-size=([^&]*)/)[1];
+        font_size = uri.match(/[&?]?font-size=([^&]*)/)[1];
+        // set font size in editor
+        document.querySelector("#font-size-range").value = font_size.match(/(\d+)px/)[1];
       }catch(e){
         font_size = getFontSize();
       }
@@ -93,7 +97,10 @@ window.onload = function() {
       // get font-family from url
       var font_family = '';
       try{
-        font_family = uri.match(/font-family=([^&]*)&/)[1];
+        font_family = uri.match(/[&?]?font-family=([^&]*)&/)[1];
+
+        // set font size in editor
+        document.querySelector("[data-id='font-family-selector']").value = font_family;
       }catch(e){
         font_family = getFontFamily();
       }
@@ -105,18 +112,37 @@ window.onload = function() {
         window.color_sequence = default_color_sequence;
       }
 
-      // get colors from url
+      // get message from url
       var message = '';
       try{
-        message = decodeURIComponent(document.location.search.match(/message=(.*)/)[1]);
+        message = decodeURIComponent(decodeURIComponent(document.location.search.match(/message=(.*)/)[1]));
+        document.querySelector('textarea').value = message;
       }catch(e){
         message = document.querySelector("textarea").getAttribute('placeholder');
       }
 
       setText( message, font_size, font_family );
-      setTextSize(font_size)
-      setFontFamily(font_family)
+      setTextSize(font_size);
+      setFontFamily(font_family);
+
+      // get colors from url
+      var edit_existing = '';
+      try{
+        edit_existing = decodeURIComponent(document.location.search.match(/[&?]?edit=true[&]?/)[1]);
+      }catch(e){
+        edit_existing = false;
+      }
+
+      setURI( font_size, font_family, message, window.color_sequence );
+
+      // option 3: all parameters and url param "edit=true" ->
+      if(edit_existing) // add default edit_existing
+      {
+        document.querySelector("[data-id='controls']").style.display = 'block';
+        addListenersForEditor();
+      }
     }
+
 
   // color spinning animation
     var angle = 0;
