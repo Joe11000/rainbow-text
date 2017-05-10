589fc7c02c0901f52922af86420bff86fa4fe983
Joe Noonan <joenoonan27@gmail.com>
2017-05-10 02:16:27 -0500

    everything is working. Still need to clean up this ugly javascript. This should have been made in react from the beginning

--- index.js
+++ index.js
@@ -50,7 +50,7 @@
   }
 
   function isTextShadowChecked() {
-    $("[data-id='text-shadow-checkbox']:checked").length == 1
+    return $("[data-id='text-shadow-checkbox']:checked").length == 1
   }
 
   function getTextShadow() {
@@ -58,8 +58,6 @@
   }
 
   function setTextShadow(val) {
-      debugger;
-
     stopSpinning();
     $("[data-id='colorful-text'] pre").css('text-shadow', `0 0 ${val}px`)
     startSpinning();
@@ -72,11 +70,13 @@
   }
 
   function setURI(args=({})) {
+    debugger
+
     let message = args['message'] || getText();
-    let font_size = args['font_size'] || getFontSize();
-    let font_family = args['font_family'] || getFontFamily();
+    let font_size = args['font-size'] || getFontSize();
+    let font_family = args['font-family'] || getFontFamily();
     let colors = args['colors'] || getColors();
-    let text_shadow = args['text_shadow'] || getTextShadow();
+    let text_shadow = args['text-shadow'] || getTextShadow();
 
     if(button_text_changed) {
       document.querySelector('#controls #output-html-wrapper button').innerHTML = " Copy URL: ";
@@ -85,10 +85,10 @@
     // only add a color string if color is not default
     let color_sequence_for_uri = '';
     if(colors != window.default_color_sequence){
-      color_sequence_for_uri = `&color_sequence=${encodeURIComponent(colors)}`;
+      color_sequence_for_uri = `&color-sequence=${encodeURIComponent(colors)}`;
     }
 
-    let text_shadow_sequence_for_uri = isTextShadowChecked() ? `&text_shadow=${getTextShadow()}` : '';
+    let text_shadow_sequence_for_uri = isTextShadowChecked() ? `&text-shadow=${getTextShadow()}` : '';
     let message_sequence_for_uri = `&message=${encodeURIComponent(encodeURIComponent(message))}`;
 
     document.querySelector("[data-id='output-html']").innerHTML = document.location.origin + document.location.pathname +
@@ -99,7 +99,6 @@
   }
 
   function getText() {
-    debugger
     return document.querySelector("[data-id='colorful-text'] pre").innerHTML;
   }
   function setText(message) {
@@ -115,19 +114,13 @@
     $textbox = document.querySelector("[data-id='controls'] textarea");
     $textbox.addEventListener('input', function(e){
       setText(this.value);
-      setURI( { message: this.value } );
+      setURI( { 'message': this.value } );
     });
 
     // font-size slider listener
     document.querySelector("[data-id='font-size-range']").addEventListener('input', function(e){
       setTextSize(this.value + 'px');
-      setURI( {'font-family': this.value + 'px' } );
-    });
-
-    // font-style listener
-    document.querySelector("[data-id='font-family-selector']").addEventListener('input', function(e){
-      setFontFamily(this.value);
-      setURI( {'font-style': this.value });
+      setURI( {'font-size': this.value + 'px' } );
     });
 
     // font-style listener
@@ -137,7 +130,6 @@
     });
 
     $("[data-id='text-shadow-checkbox']").on('change', function() {
-      debugger;
       if(this.checked){
         let val = $(this).siblings("[data-id='text-shadow-range']").val()
         setTextShadow(val);
@@ -145,12 +137,11 @@
       else {
         removeTextShadow();
       }
-      setURI( {'text-shadow': undefined } );
 
+      setURI( {'text-shadow': undefined } );
     });
 
     $("[data-id='text-shadow-range']").on('input', function() {
-      debugger
       if( !isTextShadowChecked()) {
         $("[data-id='text-shadow-checkbox']").prop( 'checked', true );
       }
@@ -174,89 +165,6 @@
     document.querySelector("[data-id='colorful-text'] pre").style.fontFamily = style;
   }
 
-
-  // option 1: no parameters -> make page display text
-    if(document.location.search === '') {
-      // show page specific
-      document.querySelector("[data-id='controls']").style.display = 'block';
-      addListenersForEditor();
-      document.querySelector("[data-id='colorful-text'] pre").style.fontFamily = getFontFamily();
-      window.color_sequence = window.default_color_sequence;
-    }
-
-  // option 2: all parameters except no url param "edit=true"
-    else
-    {
-      // edit page specific
-      var uri = decodeURIComponent(document.location.search);
-
-      var font_size = '';
-      try{
-        font_size = uri.match(/[&?]?font-size=([^&]*)/)[1];
-        // set font size in editor
-        document.querySelector("#font-size-range").value = font_size.match(/(\d+)px/)[1];
-      }catch(e){
-        font_size = getFontSize();
-      }
-
-      // get font-family from url
-      var font_family = '';
-      try{
-        font_family = uri.match(/[&?]?font-family=([^&]*)&/)[1];
-
-        // set font size in editor
-        document.querySelector("[data-id='font-family-selector']").value = font_family;
-      }catch(e){
-        font_family = getFontFamily();
-      }
-
-      // get colors from url
-      try{
-        window.color_sequence = decodeURIComponent(uri.match(/color_sequence=([^&]*)/)[1]);
-      }catch(e){
-        window.color_sequence = default_color_sequence;
-      }
-
-      // get text-shadow from url
-      var text_shadow = '';
-      try{
-        text_shadow = uri.match(/[&?]?text-shadow=([^&]*)&/)[1];
-        $("[data-id='text-shadow-checkbox']").prop( 'checked', true );
-        $("[data-id='text-shadow-range']").val(text_shadow)
-      }catch(e){}
-
-      // get message from url
-      var message = '';
-      try{
-        message = decodeURIComponent(decodeURIComponent(document.location.search.match(/message=(.*)/)[1]));
-        document.querySelector('textarea').value = message;
-      }catch(e){
-        message = document.querySelector("textarea").getAttribute('placeholder');
-      }
-
-      setText( message, font_size, font_family );
-      setTextSize(font_size);
-      setFontFamily(font_family);
-
-      // get colors from url
-      var edit_existing = '';
-      try{
-        edit_existing = decodeURIComponent(document.location.search.match(/[&?]?edit=true[&]?/)[1]);
-      }catch(e){
-        edit_existing = false;
-      }
-
-      setURI( { 'message': message, 'text_shadow': text_shadow, 'font_size': font_size, 'font_family': font_family, 'colors': window.color_sequence } );
-
-      // option 3: all parameters and url param "edit=true" ->
-      if(edit_existing) // add default edit_existing
-      {
-        document.querySelector("[data-id='controls']").style.display = 'block';
-        addListenersForEditor();
-      }
-    }
-
-
   // color spinning animation
     var angle = 0;
     var $text = document.querySelector("[data-id='colorful-text'] pre");
@@ -273,6 +181,104 @@
     window.stopSpinning = function(){
       clearInterval(window.rainbowIntervalID);
     }
+
+      // option 1: no parameters -> make page display text
+    if(document.location.search === '') {
+      debugger
+      // show page specific
+      document.querySelector("[data-id='controls']").style.display = 'block';
+      addListenersForEditor();
+      document.querySelector("[data-id='colorful-text'] pre").style.fontFamily = getFontFamily();
+      window.color_sequence = window.default_color_sequence;
+    }
+
+  // change settings inside controller
+  // option 2: all parameters except no url param "edit=true"
+
+    // option 3: at the end of option 2
+    else
+    {
+      // edit page specific
+      var uri = decodeURIComponent(document.location.search);
+
+      // get colors from url
+      var edit_existing;
+      try{
+        edit_existing = decodeURIComponent(document.location.search.match(/[&?]?edit=true[&]?/)[1]);
+      }catch(e){
+        edit_existing = false;
+      }
+
+      var font_size;
+      try{
+        font_size = uri.match(/[&?]?font-size=([^&]*)/)[1];
+      }catch(e){
+        font_size = getFontSize();
+      }
+
+      // get font-family from url
+      var font_family;
+      try{
+        font_family = uri.match(/[&?]?font-family=([^&]*)&/)[1];
+      }catch(e){
+        font_family = getFontFamily();
+      }
+
+      // get colors from url
+      try{
+        window.color_sequence = decodeURIComponent(uri.match(/color-sequence=([^&]*)/)[1]);
+      }catch(e){
+        window.color_sequence = default_color_sequence;
+      }
+
+      // get text-shadow from url
+      var text_shadow;
+      try{
+        text_shadow = uri.match(/[&?]?text-shadow=([^&]*)&/)[1];
+      }catch(e){}
+
+      // get message from url
+      var message = '';
+      try{
+        message = decodeURIComponent(decodeURIComponent(document.location.search.match(/message=(.*)/)[1]));
+      }catch(e){
+        message = document.querySelector("textarea").getAttribute('placeholder');
+      }
+
+      setText( message, font_size, font_family );
+      setTextSize(font_size);
+      setFontFamily(font_family);
+
+      if(text_shadow != undefined) {
+        setTextShadow(Number.parseInt(text_shadow));
+      }
+
+
+  // option 3: all parameters and url param "edit=true" ->
+      if(edit_existing) // add default edit_existing
+      {
+        if(text_shadow != undefined) {
+          $("[data-id='text-shadow-checkbox']").prop( 'checked', true);
+          $("[data-id='text-shadow-range']").val(Number.parseInt(text_shadow));
+        }
+        // set font size in editor
+        document.querySelector("#font-size-range").value = font_size.match(/(\d+)px/)[1];
+
+        // set font family in editor
+        document.querySelector("[data-id='font-family-selector']").value = font_family;
+
+        if(message != '' && message != undefined && message != 'Type Message Here'){
+          document.querySelector('textarea').value = message;
+        }
+
+        document.querySelector("[data-id='controls']").style.display = 'block';
+
+        setURI( { 'message': message, 'text-shadow': text_shadow, 'font-size': font_size, 'font-family': font_family, 'colors': window.color_sequence } );
+        addListenersForEditor();
+      }
+    }
+
+
     startSpinning();
 
     document.querySelector("[data-id='color-selector-wrapper'] ul").innerHTML = createColorSelectors(getColors()) + createColorButton();
@@ -329,3 +335,4 @@
       console.error('Trigger:', e.trigger);
     });
 })();
+
